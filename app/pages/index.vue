e3f3c0482a2844214d05fec8c5a9e0494a18a8d0
kevinlee-06 <cgshyuankai@proton.me>
2025-03-18 20:15:21 +0800

    refactor: clean up template structure and improve readability in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -52,84 +52,82 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>隨機餐廳選擇器</h1>
-    <button @click="getRandomRestaurant">
-      選擇餐廳
-    </button>
+  <h1>隨機餐廳選擇器</h1>
+  <button @click="getRandomRestaurant">
+    選擇餐廳
+  </button>
 
+  <div
+    v-if="selectedRestaurant"
+    class="selected-restaurant"
+  >
+    <h3>{{ selectedRestaurant.Restaurant }}</h3>
+    <p>地點: {{ selectedRestaurant.Location }}</p>
+    <p>類型: {{ selectedRestaurant.Genre }}</p>
+    <p>價格範圍: {{ selectedRestaurant.Price }}</p>
+    <p>評論: {{ selectedRestaurant.Comments }}</p>
     <div
-      v-if="selectedRestaurant"
-      class="selected-restaurant"
+      class="map"
+      style="height:400px; width:600px"
     >
-      <h3>{{ selectedRestaurant.Restaurant }}</h3>
-      <p>地點: {{ selectedRestaurant.Location }}</p>
-      <p>類型: {{ selectedRestaurant.Genre }}</p>
-      <p>價格範圍: {{ selectedRestaurant.Price }}</p>
-      <p>評論: {{ selectedRestaurant.Comments }}</p>
-      <div
-        class="map"
-        style="height:400px; width:600px"
+      <LMap
+        :center="[lat, lon]"
+        :use-global-leaflet="false"
+        :zoom="zoom"
       >
-        <LMap
-          :center="[lat, lon]"
-          :use-global-leaflet="false"
-          :zoom="zoom"
+        <LTileLayer
+          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+          layer-type="base"
+          name="OpenStreetMap"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LMarker
+          draggable
+          :lat-lng="[lat, lon]"
         >
-          <LTileLayer
-            attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-            layer-type="base"
-            name="OpenStreetMap"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <LMarker
-            draggable
-            :lat-lng="[lat, lon]"
-          >
-            <LPopup>{{ selectedRestaurant.Restaurant }}</LPopup>
-          </LMarker>
-          <LPolygon
-            id="北科校區"
-            color="#41b782"
-            :fill="true"
-            fill-color="#41b782"
-            :fill-opacity="0.5"
-            :lat-lngs="[
-              [25.042499915975604, 121.5330912641657],
-              [25.041955580123695, 121.53661032227251],
-              [25.043345575810104, 121.53627772836607],
-              [25.043714942642538, 121.53630455045088],
-              [25.043885045415383, 121.53553743869892],
-              [25.043982246893908, 121.53526653559774],
-              [25.04407458822715, 121.53466840300793],
-              [25.04431030132269, 121.53423656735129],
-              [25.044631064797045, 121.53390397344485],
-              [25.044373482075894, 121.53308321748595],
-            ]"
-          />
-        </LMap>
-      </div>
-      <br>
+          <LPopup>{{ selectedRestaurant.Restaurant }}</LPopup>
+        </LMarker>
+        <LPolygon
+          id="北科校區"
+          color="#41b782"
+          :fill="true"
+          fill-color="#41b782"
+          :fill-opacity="0.5"
+          :lat-lngs="[
+            [25.042499915975604, 121.5330912641657],
+            [25.041955580123695, 121.53661032227251],
+            [25.043345575810104, 121.53627772836607],
+            [25.043714942642538, 121.53630455045088],
+            [25.043885045415383, 121.53553743869892],
+            [25.043982246893908, 121.53526653559774],
+            [25.04407458822715, 121.53466840300793],
+            [25.04431030132269, 121.53423656735129],
+            [25.044631064797045, 121.53390397344485],
+            [25.044373482075894, 121.53308321748595],
+          ]"
+        />
+      </LMap>
     </div>
+    <br>
+  </div>
 
-    <div class="all-restaurants">
-      <h2>所有餐廳:</h2>
-      <div class="restaurant-grid">
-        <div
-          v-for="restaurant in restaurants"
-          :key="restaurant.Restaurant"
-          class="restaurant-card"
-          :class="{ selected: selectedRestaurant === restaurant }"
-          @click="toggleDetails(restaurant)"
-        >
-          <h3>{{ restaurant.Restaurant }}</h3>
-          <p>地點: {{ restaurant.Location }}</p>
-          <p>類型: {{ restaurant.Genre }}</p>
-          <p>價格範圍: {{ restaurant.Price }}</p>
-          <p v-if="selectedRestaurant === restaurant">
-            評論: {{ restaurant.Comments }}
-          </p>
-        </div>
+  <div class="all-restaurants">
+    <h2>所有餐廳:</h2>
+    <div class="restaurant-grid">
+      <div
+        v-for="restaurant in restaurants"
+        :key="restaurant.Restaurant"
+        class="restaurant-card"
+        :class="{ selected: selectedRestaurant === restaurant }"
+        @click="toggleDetails(restaurant)"
+      >
+        <h3>{{ restaurant.Restaurant }}</h3>
+        <p>地點: {{ restaurant.Location }}</p>
+        <p>類型: {{ restaurant.Genre }}</p>
+        <p>價格範圍: {{ restaurant.Price }}</p>
+        <p v-if="selectedRestaurant === restaurant">
+          評論: {{ restaurant.Comments }}
+        </p>
       </div>
     </div>
   </div>
