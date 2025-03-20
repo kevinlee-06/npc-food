987373775899fdae7ffa54b48ac75b7b740ff077
kevinlee-06 <cgshyuankai@proton.me>
2025-03-20 09:49:28 +0800

    feat: implement auto-scroll feature in settings page

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+const autoScroll = useCookie('auto-scroll', {
+  default: () => true,
+  watch: true,
+})
+</script>
+
 <template>
   <h1>settings</h1>
+  {{ autoScroll }}
+  <div>
+    <input
+      id="auto-scroll"
+      v-model="autoScroll"
+      type="checkbox"
+    >
+  </div>
 </template>
